c6f7a9829342af8a2073b6994296ee2a83b1fd84
Plinio Nascimento <plinio.gsnascimento@gmail.com>
2020-05-28 21:43:25 -0300

    First commit

---
+++ Orders.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Orders.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation(1001, "Hello world was called!");
+            _logger.LogWarning(1002, "Api doesn't have a database!");
+            return "Hello World";
+        }
+    }
+}
